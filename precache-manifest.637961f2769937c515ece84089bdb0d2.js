d2301e3c945ef1243ba6ac439327f40836984345
Daniel Bank <daniel.p.bank@gmail.com>
2019-01-05 22:42:35 -0700

    Updates

---
+++ precache-manifest.637961f2769937c515ece84089bdb0d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7724170a524cf5b6997",
+    "url": "/static/js/main.c7724170.chunk.js"
+  },
+  {
+    "revision": "d36c7a65841641b75930",
+    "url": "/static/js/1.d36c7a65.chunk.js"
+  },
+  {
+    "revision": "c7724170a524cf5b6997",
+    "url": "/static/css/main.165945b3.chunk.css"
+  },
+  {
+    "revision": "e71d8d5519e44e456ed453327ac23eaf",
+    "url": "/index.html"
+  }
+];
